4e6a4c926a1dce76b931c5ed37da39614049cf57
Gabriel Medeiros <gabrielmedeirosvino@gmail.com>
2023-03-03 13:23:09 -0300

    fix: page functionality

--- src/components/paymentMethod/paymentInPerson.js
+++ src/components/paymentMethod/paymentInPerson.js
@@ -2,17 +2,35 @@ import { useState } from 'react';
 import styled from 'styled-components';
 
 export default function PaymentInPerson(props) {
-  const { setWithOrWithoutHotel } = props;
+  const { setWithOrWithoutHotel, setTotal } = props;
+  const [withHotel, setWithHotel] = useState('');
+  const [withoutHotel, setWithoutHotel] = useState('');
   return (
     <>
       <PaymentContainer>
         <p>Ótimo! Agora escolha sua modalidade de hospedagem</p>
         <Methods>
-          <InPersonMethod onClick={() => setWithOrWithoutHotel(true)}>
+          <InPersonMethod
+            backgroud={withoutHotel}
+            onClick={() => {
+              setWithOrWithoutHotel(true);
+              setWithoutHotel('#FFEED2');
+              setWithHotel('');
+              setTotal(250);
+            }}
+          >
             <p>Sem Hotel</p>
             <p>R$ 0</p>
           </InPersonMethod>
-          <OnlineMethod onClick={() => setWithOrWithoutHotel(true)}>
+          <OnlineMethod
+            backgroud={withHotel}
+            onClick={() => {
+              setWithOrWithoutHotel(true);
+              setTotal(600);
+              setWithHotel('#FFEED2');
+              setWithoutHotel('');
+            }}
+          >
             <p>Com hotel</p>
             <p>+ R$ 350</p>
           </OnlineMethod>
@@ -44,13 +62,13 @@ const InPersonMethod = styled.div`
   justify-content: center;
   align-items: center;
   margin-right: 24px;
-
+  background-color: ${props => props.backgroud};
   p:nth-child(1) {
     color: #454545;
   }
 
   :hover {
-    background-color: #ccc;
+    background-color: #FFEED2;
   }
 
   cursor: pointer;
@@ -65,13 +83,14 @@ const OnlineMethod = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-color: ${props => props.backgroud};
 
   p:nth-child(1) {
     color: #454545;
   }
 
   :hover {
-    background-color: #ccc;
+    background-color: #FFEED2;
   }
 
   cursor: pointer;
