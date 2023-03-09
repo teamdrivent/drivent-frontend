661bafeb4c50028a45e9544c83f62c9c8d0c6f5c
oscarfgmaia <oscarfgmaia@gmail.com>
2023-03-09 14:50:28 -0300

    fix: update dashboard when ticket is paid

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -2,19 +2,25 @@ import { useEffect, useState } from 'react';
 import CardPayment from '../../../components/paymentMethod/cardPayment.js';
 import PaymentMethod from '../../../components/paymentMethod/paymentMethod.js';
 import useTicket from '../../../hooks/api/useTicket.js';
-
+import axios from 'axios';
+import useToken from '../../../hooks/useToken.js';
 export default function Payment() {
-  let tempTicket = useTicket().ticket;
-  const [ticket, setTicket] = useState(tempTicket);
-  console.log(ticket);
-  useEffect(() => {}, [ticket]);
+  const token = useToken();
+  const [ticket, setTicket] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_BASE_URL}/tickets`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setTicket(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <>
-      {ticket !== null ? (
-        <CardPayment paid={ticket.status === 'RESERVED'} />
-      ) : (
-        <PaymentMethod ticket={ticket} setTicket={setTicket} />
-      )}
-    </>
+    <>{ticket !== null ? <CardPayment ticket={ticket} /> : <PaymentMethod ticket={ticket} setTicket={setTicket} />}</>
   );
 }
