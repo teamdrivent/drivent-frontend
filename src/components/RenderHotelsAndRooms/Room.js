993b32c0a836578283d09c2b16259bfeb48c7590
oscarfgmaia <oscarfgmaia@gmail.com>
2023-03-12 02:55:50 -0300

    style: format text

--- src/components/RenderHotelsAndRooms/Room.js
+++ src/components/RenderHotelsAndRooms/Room.js
@@ -4,24 +4,34 @@ import { HiOutlineUser } from 'react-icons/hi';
 export default function Room({ data }) {
   const [select, setSelect] = useState(false);
   return (
-    <OneRoom onClick={() => select===false?setSelect(true):setSelect(false)} bool={select}>{data.name}<div>{data.capacity === 2 ?<><HiOutlineUser fontSize={'22.25px'}/><HiOutlineUser fontSize={'22.25px'}/></>:<HiOutlineUser fontSize={'22.25px'}/>}</div></OneRoom>
+    <OneRoom onClick={() => (select === false ? setSelect(true) : setSelect(false))} bool={select}>
+      {data.name}
+      <div>
+        {data.capacity === 2 ? (
+          <>
+            <HiOutlineUser fontSize={'22.25px'} />
+            <HiOutlineUser fontSize={'22.25px'} />
+          </>
+        ) : (
+          <HiOutlineUser fontSize={'22.25px'} />
+        )}
+      </div>
+    </OneRoom>
   );
 }
 
 const OneRoom = styled.div`
-
-width:190px;
-height:45px;
-border: 1px solid #CECECE;
-border-radius: 10px;
-display:flex;
-margin-right: 20px;
-align-items:center;
-justify-content:space-between;
-font-size: 20px;
-color:#454545;
-font-weight: 700;
-padding:10px;
-background-color:${props => props.bool ===false?null:'#FFEED2'};
-`
-;
+  width: 190px;
+  height: 45px;
+  border: 1px solid #cecece;
+  border-radius: 10px;
+  display: flex;
+  margin-right: 20px;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 20px;
+  color: #454545;
+  font-weight: 700;
+  padding: 10px;
+  background-color: ${(props) => (props.bool === false ? null : '#FFEED2')};
+`;
