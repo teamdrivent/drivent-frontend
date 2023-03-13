374d27ebca30b20f55bf1d18a2e48922db3b0262
matheuslimaOS <souza_matheus@discente.ufg.br>
2023-03-13 09:19:13 -0300

    feat: option to change room after the reservation

--- src/components/RenderHotelsAndRooms/RoomsList.js
+++ src/components/RenderHotelsAndRooms/RoomsList.js
@@ -1,9 +1,35 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import Room from './Room';
-export default function RoomsList({ HotelId, HotelRooms }) {
+import axios from 'axios';
+import useToken from '../../hooks/useToken';
+
+export default function RoomsList({ bookingId, setBookingId, setBooking, HotelId, HotelRooms }) {
   const filterRooms = HotelRooms.filter((e) => e.hotelId === HotelId);
   const [selectRooms, setSelectRooms] = useState();
+  const token = useToken();
+
+  async function booking() {
+    if(bookingId !== false) {
+      axios
+        .put(`${process.env.REACT_APP_API_BASE_URL}/booking/` + bookingId, { roomId: filterRooms[selectRooms].id }, { headers: { Authorization: `Bearer ${token}` } })
+        .then((res) => {
+          setBooking(res.data);
+          setBookingId(false);
+        })
+        .catch((err) => console.log(err));
+    }
+    else {
+      axios
+        .post(`${process.env.REACT_APP_API_BASE_URL}/booking`, { roomId: filterRooms[selectRooms].id }, { headers: { Authorization: `Bearer ${token}` } })
+        .then((res) => {
+          setBooking(res.data);
+          setBookingId(false);
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
   return (
     <Rooms>
       <h2>Ótima pedida! Agora escolha seu quarto:</h2>
@@ -12,9 +38,28 @@ export default function RoomsList({ HotelId, HotelRooms }) {
           return <Room data={e} key={i} index={i} setSelectRooms={setSelectRooms} selectRooms={selectRooms} selected={selectRooms===i } />;
         })}
       </AllRooms>
+      {
+        selectRooms !== undefined ? 
+          <ChangeButton onClick={() => {booking();}}>
+            RESERVAR QUARTO
+          </ChangeButton> :
+          ''
+      }
     </Rooms>
   );
 }
+const ChangeButton = styled.button`
+  width: 182px;
+  height: 37px;
+  background: #E0E0E0;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  border:none;
+  margin-top: 45px;
+  margin-bottom: 120px;
+  cursor: pointer;
+`;
+
 const Rooms = styled.div`
   h2 {
     font-size: 20px;
@@ -26,5 +71,4 @@ const Rooms = styled.div`
 const AllRooms = styled.div`
   display: flex;
   flex-wrap: wrap;
-  height: 220px;
 `;
--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -5,24 +5,39 @@ import WithPay from '../../../components/Hotel/withPay';
 import useTicket from '../../../hooks/api/useTicket';
 import useToken from '../../../hooks/useToken';
 import axios from 'axios';
+import AlreadyBooked from '../../../components/Hotel/AlreadyBooked';
+
 export default function Hotel() {
   const token = useToken();
   const [ticket, setTicket] = useState(null);
+  const [booking, setBooking] = useState(null);
+  const [bookingId, setBookingId] = useState(false);
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_BASE_URL}/tickets`, { headers: { Authorization: `Bearer ${token}` } })
       .then((res) => {
-        console.log(res.data);
         setTicket(res.data);
+      })
+      .catch((err) => console.log(err));
+    axios
+      .get(`${process.env.REACT_APP_API_BASE_URL}/booking`, { headers: { Authorization: `Bearer ${token}` } })
+      .then((res) => {
+        setBooking(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <>
-      {ticket?.ticketTypeId === 1 || ticket?.ticketTypeId === 3 ? <WithoutHotel /> : ''}
-      {ticket?.status !== 'PAID' ? <WithoutPay /> : ''}
-      {ticket?.ticketTypeId === 2 && ticket?.status === 'PAID' ? <WithPay /> : ''}
+      {
+        booking == undefined || bookingId !== false? 
+          (
+            (ticket?.ticketTypeId === 1 || ticket?.ticketTypeId === 3 ? <WithoutHotel /> : ''),
+            (ticket?.status !== 'PAID' ? <WithoutPay /> : ''),
+            (ticket?.ticketTypeId === 2 && ticket?.status === 'PAID' ? <WithPay bookingId={bookingId} setBooking={setBooking} setBookingId={setBookingId} /> : '')
+          ) : 
+          <AlreadyBooked setBookingId={setBookingId}/>
+      }
     </>
   );
 }
